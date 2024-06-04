d9e82c3a0724bcf308e3794dcb89da54dba5cf53
SWP-Jewelry-Sales-System <hoanvngoc2003@gmail.com>
2024-06-04 10:54:58 +0700

    update

--- code/JewelSystemBE/obj/Release/net8.0/JewelSystemBE.AssemblyInfo.cs
+++ code/JewelSystemBE/obj/Release/net8.0/JewelSystemBE.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelSystemBE")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19e0f25353f60acf63fe725bd9cd0f0cbc05bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelSystemBE")]
