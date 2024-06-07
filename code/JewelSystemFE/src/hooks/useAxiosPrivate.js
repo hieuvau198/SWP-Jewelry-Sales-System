36c60b6cb25c2aa7fc76524b0ab1028c60190c77
bibinhoxok <bibinhoxok@gmail.com>
2024-06-07 21:31:32 +0700

    fe update

--- code/JewelSystemFE/src/hooks/useAxiosPrivate.js
+++ code/JewelSystemFE/src/hooks/useAxiosPrivate.js
@@ -8,7 +8,7 @@ const useAxiosPrivate = () => {
     const { auth } = useAuth();
 
     useEffect(() => {
-
+        console.log(auth);
         const requestIntercept = axiosPrivate.interceptors.request.use(
             config => {
                 if (!config.headers['Authorization']) {
--- code/JewelSystemFE/src/index.js
+++ code/JewelSystemFE/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import "./assets/scss/main.scss";
 import { AuthProvider } from './context/AuthProvider';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
