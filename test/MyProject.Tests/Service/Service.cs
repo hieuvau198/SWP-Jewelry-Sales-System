2d53ad80f943d1a28a019a72d9c93472c2fb8a6b
AKAVINAMIU <nammchype@gmail.com>
2024-06-09 12:14:43 +0700

    New testing
    update new testing

---
+++ test/MyProject.Tests/Service/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Tests.Service
+{
+    public class MyService : IMyService
+    {
+        public string GetData()
+        {
+            // Logic để lấy dữ liệu từ nơi nào đó
+            return "Real Data";
+        }
+    }
+}
