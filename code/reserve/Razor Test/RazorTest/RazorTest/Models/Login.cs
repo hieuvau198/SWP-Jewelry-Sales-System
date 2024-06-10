99b9cdbcb533e9067ff7957599ffaee7a32d9b89
SWP-Jewelry-Sales-System <hoanvngoc2003@gmail.com>
2024-06-10 18:33:20 +0700

    updtae

---
+++ code/reserve/Razor Test/RazorTest/RazorTest/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorTest.Models
+{
+    public class Login
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
