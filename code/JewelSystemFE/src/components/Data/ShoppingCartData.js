7067614f066f259cfdb12d61d73bdacbbac7a314
bibinhoxok <bibinhoxok@gmail.com>
2024-06-18 05:49:47 +0700

    update cart

--- code/JewelSystemFE/src/components/Data/ShoppingCartData.js
+++ code/JewelSystemFE/src/components/Data/ShoppingCartData.js
@@ -13,7 +13,7 @@ export const ShoppingCartData = () =>{
 
 
     const {items, updateItemQuantity, removeItem, getItem, emptyCart, cartTotal} = useCart();
-    const totalPrice = (price,quantity) => {return price*quantity};
+    const totalPrice = (price,quantity) => {return (price*quantity)+'$'};
 
 
     return({
@@ -42,7 +42,7 @@ export const ShoppingCartData = () =>{
             },
             {
                 name: "PRICE",
-                selector: (row) => row.price,
+                selector: (row) => row.price+'$',
                 sortable: true
             },
             {
