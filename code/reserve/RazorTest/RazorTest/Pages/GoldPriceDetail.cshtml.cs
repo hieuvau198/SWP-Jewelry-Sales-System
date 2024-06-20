cfc5c194f58fb588e2ec0984f006c6b567863be1
SWP-Jewelry-Sales-System <hoanvngoc2003@gmail.com>
2024-06-20 17:13:52 +0700

    update Gold display, Template Razor Test

--- code/reserve/RazorTest/RazorTest/Pages/GoldPriceDetail.cshtml.cs
+++ code/reserve/RazorTest/RazorTest/Pages/GoldPriceDetail.cshtml.cs
@@ -1,32 +1,32 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorTest.Models;
-using RazorTest.Services;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+//using RazorTest.Services;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace RazorTest.Pages
-{
-    public class GoldPriceListModel : PageModel
-    {
-        private readonly ApiService _apiService;
+//namespace RazorTest.Pages
+//{
+//    public class GoldListModel : PageModel
+//    {
+//        private readonly ApiService _apiService;
 
-        public GoldPriceListModel(ApiService apiService)
-        {
-            _apiService = apiService;
-        }
+//        public GoldListModel(ApiService apiService)
+//        {
+//            _apiService = apiService;
+//        }
 
-        public List<GoldPrice> GoldPrices { get; set; }
+//        public List<GoldPrice> Golds { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            var goldprices = await _apiService.GetAsync<List<GoldPrice>>("http://localhost:5071/api/goldprice");
+//        public async Task OnGetAsync()
+//        {
+//            var golds = await _apiService.GetAsync<List<GoldPrice>>("http://localhost:5071/api/gold");
 
-            if (goldprices != null)
-            {
-                GoldPrices = goldprices.OrderBy(c => c.Name).ToList();
-            }
-        }
+//            if (golds != null)
+//            {
+//                Golds = golds.OrderBy(c => c.Name).ToList();
+//            }
+//        }
 
-    }
-}
+//    }
+//}
