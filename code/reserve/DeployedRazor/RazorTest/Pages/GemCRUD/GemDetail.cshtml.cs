4e01eb432d7e22082095bfc83d38808592878b8b
SWP-Jewelry-Sales-System <hoanvngoc2003@gmail.com>
2024-07-02 15:05:40 +0700

    fix design

--- code/reserve/DeployedRazor/RazorTest/Pages/GemCRUD/GemDetail.cshtml.cs
+++ code/reserve/DeployedRazor/RazorTest/Pages/GemCRUD/GemDetail.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorTest.Models;
 using RazorTest.Services;
+using RazorTest.Utilities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,15 +17,20 @@ namespace RazorTest.Pages.GemCRUD
             _apiService = apiService;
         }
 
-        public List<Gem> Gems { get; set; }
+  //      public List<Gem> Gems { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Gem> Gems { get; set; }
+
+        private const int PageSize = 15;
+
+        public async Task OnGetAsync(int currentPage = 1)
         {
             var gems = await _apiService.GetAsync<List<Gem>>("https://jewelsystembe20240701213216.azurewebsites.net/api/gem");
 
             if (gems != null)
             {
-                Gems = gems.OrderBy(g => g.GemId).ToList();
+                Gems = PaginatedList<Gem>.Create(gems.AsQueryable(), currentPage, 15);
+
             }
         }
     }
--- code/reserve/DeployedRazor/RazorTest/obj/Debug/net8.0/RazorTest.AssemblyInfo.cs
+++ code/reserve/DeployedRazor/RazorTest/obj/Debug/net8.0/RazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e3e69ef880894f9010e2a244d3dd99a00c282fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
