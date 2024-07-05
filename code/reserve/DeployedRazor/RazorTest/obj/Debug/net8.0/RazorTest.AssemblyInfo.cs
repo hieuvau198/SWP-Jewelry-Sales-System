d1bba991af0f91c73dd47265a30562cca247d938
hieuvau198 <nhatduongfpt2000@gmail.com>
2024-07-06 05:26:02 +0700

    Update Publish Profile and some Internal changes

--- code/reserve/DeployedRazor/RazorTest/obj/Debug/net8.0/RazorTest.AssemblyInfo.cs
+++ code/reserve/DeployedRazor/RazorTest/obj/Debug/net8.0/RazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4ccaa32b7dd3ab0eaee5de8dfe2a81b2612e52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
